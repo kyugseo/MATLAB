f4a9afc7973f2dc54131ef360275c25c203e1510
kyugseo <101443182+kyugseo@users.noreply.github.com>
2022-06-23 12:25:17 -0700

    Update and rename Q2.m to Vandermonde matrix.m

--- matlab/Runge phenomenon/Q2.m
+++
@@ -1,9 +0,0 @@
-n = 1001;
-V = zeros(n+1,n+1);
-x = linspace(-1,1,n+1);
-for i=1:n+1
-% Set up row i.
-    for j=1:n+1
-    V(i,j) = x(i)^(j-1);
-    end
-end
---
+++ matlab/Runge phenomenon/Vandermonde matrix.m
@@ -0,0 +1,12 @@
+% For n + 1 interpolation points x0, x1, ..., xn, implement the following forms of the interpolating polynomial.
+% Write a Matlab function that solves the interpolation problem by the Vandermonde matrix
+
+n = 1001;
+V = zeros(n+1,n+1);
+x = linspace(-1,1,n+1);
+for i=1:n+1
+% Set up row i.
+    for j=1:n+1
+    V(i,j) = x(i)^(j-1);
+    end
+end

1af0c3119d0866bb0dd2fd911c1c19b4484e44c1
kyugseo <101443182+kyugseo@users.noreply.github.com>
2022-06-23 12:01:07 -0700

    Update and rename Q4_c.m to polynomial_eigenvalueOfA.m

---
+++ matlab/Perturbation and eigenvalues/polynomial_eigenvalueOfA.m
@@ -0,0 +1,21 @@
+% Compute the eigenvalues of A and compare the eigenvalues with r1, r2, ..., rn. 
+% Report the observations about eigenvalues and condition number of A. 
+% Compute the eigenvalues of A +(\epsilon)Qi for the 100 perturbations created in Question 1. 
+% Note that these eigenvalues may be complex numbers. 
+% Plot all the eigenvalues of the 100 matrices (i.e. A + Qi) on one plot by the command plot(eigenvalues, ’.’). 
+% Report the plot and your observations. z^n with r_{1}, r_{2}, ..., r_{n} as its roots by the Matlab command poly.
+
+n=50;
+M = 2*rand(n) - 1;
+[W,R] = qr(M); 
+B = W*diag(r)*W';
+for i = 1:100;
+    C = B+eQi(:,:,i);
+    D = B+eQiQt(:,:,i); 
+end
+
+ec = eig(C)
+ed = eig(D)
+eA = eig(A)
+eB = eig(B)
+

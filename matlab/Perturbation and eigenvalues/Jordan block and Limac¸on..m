804df366c2c3c3867afa2df60b60f8ac43c0c81a
kyugseo <101443182+kyugseo@users.noreply.github.com>
2022-06-23 12:12:21 -0700

    Update Jordan block and Limac¸on..m

--- matlab/Perturbation and eigenvalues/Jordan block and Limac¸on..m
+++ matlab/Perturbation and eigenvalues/Jordan block and Limac¸on..m
@@ -2,6 +2,7 @@ n=50; % in this case n = 50
 v =ones(1,n-1);  
 J = diag(v,1); % one on the first super diagonal
 A = 4*J;
+eQi= zeros(n,n,100)
 for i = 1:100; %100 iterations
     B = A+eQi(:,:,i); %eQi from Q1
     
